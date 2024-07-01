32540f2c6e8d10b938553d660d021d28f941f5e3
Vusal Atashov <156101309+Vusal-Atashov@users.noreply.github.com>
2024-07-01 20:20:33 +0400

    Servlet

---
+++ src/main/java/az/edu/turing/servlet/GreetingServlet.java
@@ -0,0 +1,21 @@
+package az.edu.turing.servlet;
+
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+import java.io.OutputStream;
+
+public class GreetingServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+        String pathInfo = req.getPathInfo();
+        String name = (pathInfo != null && pathInfo.length() > 1) ? pathInfo.substring(1) : "Unknown";
+
+        resp.setContentType("text/plain");
+        try (OutputStream os = resp.getOutputStream()) {
+            os.write(("Hello, " + name + " Java EE").getBytes());
+        }
+    }
+}
